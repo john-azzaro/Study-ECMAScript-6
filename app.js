0ac8ff91c6314c663a40aa6129696b2210fea96f
John Azzaro <jazzaro@temple.edu>
2019-11-28 15:37:46 -0800

    updated image code

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ function imageGallery() {                                                    // 
   const highlight = document.querySelector('.gallery-highlight');            // Select .gallery-highlight as highlight...gallery-highlight
   const previews = document.querySelectorAll('.gallery-preview img');        // Select all images from gallery previews
 
-  previews.forEach(function(preview) {
-    preview.addEventListener('click', function(event) {
-      console.log(preview)
+  previews.forEach(function(preview) {                                       // For each image in the gallery previews
+    preview.addEventListener('click', function(event) {                      // add an event listener that on click...
+      const smallSrc = this.src;                                             // returns the value of the src attribute of the image (i.e. )
+      const bigSrc = smallSrc.replace('small', 'large');                     // bigSrc is simply replacing the "small" part of the src text with "large" 
+      highlight.src = bigSrc;                                                // and finally, the large highlight picture at the top of the previews source will be the bigSrc!      
+      previews.forEach(function(preview) {                                   // And for each preview remove the active stylization
+        preview.classList.remove('city-active')
+      });
+      preview.classList.add('city-active')                                   // And add the city active class.
     });
   });
 }
