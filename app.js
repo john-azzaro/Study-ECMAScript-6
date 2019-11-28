75b9cb8a07642ef97d23cc82dd507e9010e17a39
John Azzaro <jazzaro@temple.edu>
2019-11-28 13:55:20 -0800

    fixed event listener issue

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 
 function imageGallery() {                                                    // When imageGallery runs...
   const highlight = document.querySelector('.gallery-highlight');            // Select .gallery-highlight as highlight...gallery-highlight
-  const previews = document.querySelectorAll('')                             // Select all images from previews
+  const previews = document.querySelectorAll('.gallery-preview img');        // Select all images from gallery previews
 
+  previews.forEach(function(preview) {
+    preview.addEventListener('click', function(event) {
+      console.log(preview)
+    });
+  });
 }
 
 function initialize() {                                                     // On intialization...
   imageGallery();                                                           // Run imageGallery
 }
 
-$(initialize);                                                              // Initialize app
+initialize();                                                              // Initialize app
